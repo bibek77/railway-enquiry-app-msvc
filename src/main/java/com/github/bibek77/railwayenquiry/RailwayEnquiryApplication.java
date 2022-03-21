bec35a864b231330a37d0ad17f51dac89c6a6e18
bibek77 <bibekrana01@gmail.com>
2022-03-21 21:48:03 +0530

    initial setup

---
+++ src/main/java/com/github/bibek77/railwayenquiry/RailwayEnquiryApplication.java
@@ -0,0 +1,13 @@
+package com.github.bibek77.railwayenquiry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayEnquiryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayEnquiryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/bibek77/railwayenquiry/RailwayEnquiryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bibek77.railwayenquiry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayEnquiryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
