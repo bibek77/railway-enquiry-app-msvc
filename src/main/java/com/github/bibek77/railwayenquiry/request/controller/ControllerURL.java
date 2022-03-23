468518f1a4f6db98ffb05f6fe3d5bd7316ee3cc0
bibek77 <bibekrana01@gmail.com>
2022-03-23 21:33:35 +0530

    api calls configured

---
+++ src/main/java/com/github/bibek77/railwayenquiry/request/controller/ControllerURL.java
@@ -0,0 +1,10 @@
+package com.github.bibek77.railwayenquiry.request.controller;
+
+/**
+ * @author bibek
+ */
+public interface ControllerURL {
+
+    String CUSTOM_HEALTH_CHECK_API = "/v1.0/railapi/health";
+    String PNR_RAILWAY_ENQUIRY_API = "/v1.0/railapi/pnr/{pnr}";
+}
